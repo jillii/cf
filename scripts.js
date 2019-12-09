9ee5b3b0be74a95860357f903dad1e49d4d9a071
jill <jho3292@gmail.com>
2019-12-08 23:41:21 -0500

    Update 120819.

--- scripts.js
+++ scripts.js
@@ -20,7 +20,27 @@ $(window).scroll(function(){
 
 });
 
-$(document).ready(function(){
-	$url = "https://www.facebook.com/pg/cakefact/events/";
-	console.log($.get($url));
-});
+// $(document).ready(function(){
+// 	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+// 	const url = "https://www.facebook.com/pg/cakefact/events/"; // site that doesn’t send Access-Control-*
+
+// 	$data = "";
+
+// 	$.get((proxyurl + url), function(data){
+// 	  // alert("Data: " + data);
+
+// 	  // $data = $($.parseHTML(data));
+// 	  $html_string = $(data);
+// 	  $title = $('._50f7', $html_string);
+// 	  console.log($data);
+// 	  $4dmk = $html_string.find('._50f7');
+// 	  // $title = $4dmk.find('. _50f7');
+// 	  console.log($html_string);
+// 	  console.log($title);
+
+// 	});
+
+// 	// console.log($data.find('._4dmk'));
+
+
+// });
