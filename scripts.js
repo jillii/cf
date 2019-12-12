2d6f962f0f974773961d302d9aaeb895271392ea
jill <jho3292@gmail.com>
2019-12-11 23:49:14 -0500

    Update 121119.

--- scripts.js
+++ scripts.js
@@ -7,11 +7,25 @@ console.log(" (       ) \\\\   ");
 console.log("   O   O   //   ");
 
 $('.page-link').click(function() {
-
 	$this = $(this);
 	$id = $this.attr("href");
 	$el_w_id = $($id);
 
-	$('section.active').removeClass('active');
-	$el_w_id.addClass('active');
+	scroll_to($el_w_id);
+
 });
+
+$(document).ready(function(){
+	$target = window.location.hash;
+
+	if ($target) {
+		$section = $($target);
+		scroll_to($section);
+	}
+});
+
+function scroll_to($section) {
+
+	$('section.active').removeClass('active');
+	$section.addClass('active');	
+}
